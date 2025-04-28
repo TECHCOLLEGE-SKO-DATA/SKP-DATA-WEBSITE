04330a7d7fb47d0b6d73f26b126fd1e233e1381b
Emil Hytting <emilmm21@gmail.com>
2025-04-28 14:54:01 +0200

    ....

--- wwwroot/scripts/carousel.js
+++ wwwroot/scripts/carousel.js
@@ -1,15 +1,23 @@
-
 let slideIndex = 0;
-showSlides();
+let slides;
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    slides = document.getElementsByClassName("mySlides");
+    if (slides.length > 0) {
+        showSlides();
+    }
+});
 
 function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  slides[slideIndex-1].style.display = "block";  
-  setTimeout(showSlides, 10000); // Change if mil secs aint good enough
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.opacity = "0";
+        slides[i].style.transition = "opacity 1.5s ease"; 
+    }
+
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+
+    slides[slideIndex - 1].style.opacity = "1";
+
+    setTimeout(showSlides, 7000);
 }
